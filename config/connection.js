ab542a8165f19acbdb5a082d14232728000ca4a3
dlscoccia <dlscoccia@gmail.com>
2021-09-28 14:21:28 -0500

    updating db connection method

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,13 @@
 const mysql = require('mysql')
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: 'us-cdbr-east-04.cleardb.com',
   user: 'b9c7a14a19a559',
   password: '4878bc24',
   database: 'heroku_e80dce80cfe2935'
 })
 
-connection.connect((err) => {
+connection.getConnection((err) => {
   if (err) {
     console.log(err)
   } else {
